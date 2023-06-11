cf0a086d6b71c09ffdb6de73721a57c91a6118ba
Arnoldo Suarez <arsubo@gmail.com>
2023-06-11 12:43:57 -0600

    Animacion 2D

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
-  const DiceRoller({super.key});
+  const DiceRoller({Key? key}) : super(key: key);
 
   @override
   State<DiceRoller> createState() {
@@ -12,23 +12,62 @@ class DiceRoller extends StatefulWidget {
   }
 }
 
-class _DiceRollerState extends State<DiceRoller> {
-  var currentDiceRoll = 2;
+class _DiceRollerState extends State<DiceRoller>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  int _currentDiceRoll = 2;
 
-  void rollDice() {
-    setState(() {
-      currentDiceRoll = randomizer.nextInt(6) + 1;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _animation = Tween<double>(begin: 0, end: 2 * pi).animate(_controller);
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          _currentDiceRoll = randomizer.nextInt(6) + 1;
+        });
+      }
     });
   }
 
   @override
-  Widget build(context) {
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void rollDice() {
+    _controller.reset();
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Image.asset(
-          'assets/images/dice-$currentDiceRoll.png',
-          width: 200,
+        AnimatedBuilder(
+          animation: _controller,
+          builder: (BuildContext context, Widget? child) {
+            return Transform.rotate(
+              angle: _animation.value,
+              child: Image.asset(
+                'assets/images/dice-${_currentDiceRoll.toString()}.png',
+                width: 200,
+              ),
+            );
+          },
         ),
         const SizedBox(
           height: 20,
